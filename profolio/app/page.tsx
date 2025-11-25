e95f610257bcb6a779635fbb6905861fea85669d
Xinzhu99 <jxinzhu@gmail.com>
2025-11-25 16:18:57 +0100

    WIP: page list des projets

--- profolio/app/page.tsx
+++ profolio/app/page.tsx
@@ -1,7 +1,22 @@
+import { db } from "@/lib/db/drizzle";
 import HomepageClient from "./components/HomepageClient";
+import { projects, classes, categories } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
 
-export default function Home() {
+export default async function Home() {
+  const data = await db.select({
+    id: projects.id,
+    class:classes.name,
+    name: projects.title,
+    date: projects.published_at,
+    category: categories.name
+
+  }).from(projects).leftJoin(classes, eq(classes.id, projects.class_id))
+  .leftJoin(categories, eq(categories.id, projects.category_id))
+  console.log(data)
   return (
-    <HomepageClient />
+    <div>
+
+    </div>
   );
 }
