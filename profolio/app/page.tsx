3c9d9513f042c521a28461a5bceb8d1c6963507a
Xinzhu99 <jxinzhu@gmail.com>
2025-11-26 14:48:48 +0100

    update: affichage list des projets avec SQL agglomération

--- profolio/app/page.tsx
+++ profolio/app/page.tsx
@@ -2,21 +2,45 @@ import { db } from "@/lib/db/drizzle";
 import HomepageClient from "./components/HomepageClient";
 import { projects, classes, categories } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
+import { sql } from "drizzle-orm";
 
 export default async function Home() {
-  const data = await db.select({
-    id: projects.id,
-    class:classes.name,
-    name: projects.title,
-    date: projects.published_at,
-    category: categories.name
+  const data = await db.execute(sql`
+        SELECT 
+      cat.name as category,
+      JSON_AGG(
+        JSON_BUILD_OBJECT(
+          'id', p.id,
+          'name', p.title,
+          'class', c.name,
+          'date', p.published_at
+        ) ORDER BY p.published_at DESC
+      ) as projects
+    FROM projects p
+    LEFT JOIN classes c ON c.id = p.class_id
+    LEFT JOIN categories cat ON cat.id = p.category_id
+    GROUP BY cat.name
+    ORDER BY cat.name
+    `);
 
-  }).from(projects).leftJoin(classes, eq(classes.id, projects.class_id))
-  .leftJoin(categories, eq(categories.id, projects.category_id))
-  console.log(data)
+  console.log(data.rows);
   return (
     <div>
+      {data.rows.map((row)=> (
+        <div dummy key={row.category} className="p-6">
+          <h1 className="text-blue-700 text-3xl">{row.category}</h1>
+          <div className="flex gap-4 p-4 flex-wrap">
+             {row.projects.map((project)=> (
+            <div key={project.id}>
+              <h2 className="text-2xl">{project.name} : {project.class} le {project.date}</h2>
+              <img className="w-100 h-100"src="https://cdn.dummyjson.com/recipe-images/1.webp"/>
+            </div>
+          )
+          )}
+          </div>
+          </div>
 
+      ))}
     </div>
   );
 }
